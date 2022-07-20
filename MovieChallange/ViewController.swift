c142ec6614e6299960cd3b83ecb07992b68063f3
farukkisikmt <faruk.kisik@technoface.com.tr>
2022-07-20 22:59:17 +0300

    Initial Commit

---
+++ MovieChallange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieChallange
+//
+//  Created by Ömer Faruk KISIK on 20.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
